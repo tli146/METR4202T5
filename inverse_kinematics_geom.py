3cbbed66e6efe75932ca05b8936a098aad5032fa
Ethan Waugh <etan.waugh@gmail.com>
2022-10-16 11:33:17 +1000

    Analytical inv kin can handle larger distances

--- inverse_kinematics_geom.py
+++ inverse_kinematics_geom.py
@@ -6,59 +6,50 @@ import modern_robotics as mr
 #from geometry_msgs.msg import Pose
 
 
-""" Take an array of positions and orientations and publish a list of joint angles (4 angles) """
+""" Take an array of positions and turn them into a list of joint angles (4 angles) """
 
-# Do the inverse kinematics math to return a list of joint angles
-# Get the robot to do those angles
-
-""" ROBOT DIMENSION CONSTANTS (mm)"""
+# == ROBOT DIMENSION CONSTANTS ==
 L1 = 50
 L2 = 51
 L3 = 117
 L4 = 95
 L5 = 95
 deltaY = 17
+# ===============================
 
-# subscribe for this
-desired_pos = [0, -90, 20]
+# subscribe to topic for this
+desired_pos = [0, -250, 20]
+
 desired_x, desired_y, desired_z = desired_pos
 desired_r = np.sqrt(desired_x**2 + desired_y**2)
-
+    
 def inverse_kin_analytical():
-    # Ease of notation
+    # desired x,y and z
     dx, dy, dz = desired_pos
+    # desired distance to robot
     dr = desired_r
-    print(dr)
+    eas = [np.pi/2, 3*np.pi/8, np.pi/4] # End effector angles (with horizontal axis)
     # cos theta_3
-    ctheta3 = (dr**2 + (dz+L5-L1-L2)**2 - L3**2 - L4**2) / (2*L3*L4)
-    print(ctheta3)
-    theta3 = np.arctan2(np.sqrt(1-(ctheta3)**2), ctheta3)
-    print(theta3)
-    theta2 = np.arctan2(dr, (dz+L5-L1-L2)) - np.arctan2(L4*np.sin(theta3), L3+L4*np.cos(theta3))
-    print(np.arctan2(dr, dz))
-    print(np.arctan2(L4*np.sin(theta3), L3+L4*np.cos(theta3)))
-    print(ctheta3)
-    print(np.cos(theta3))
-    theta4 = np.pi - theta2 - theta3
-    theta1 = np.arctan2(dx, -dy)
-    print([theta1,theta2,theta3,theta4])
-    
-def inverse_kin_analytical2():
-    # Ease of notation
-    dx, dy, dz = desired_pos
-    dr = desired_r
     ea = np.pi/2
-    # cos theta_3
-    ctheta3 = ((dr - L5*np.cos(ea))**2 + (dz+L5*np.sin(ea)-L1-L2)**2 - L3**2 - L4**2) \
-        / (2*L3*L4)
+    # Iterate through list of end angles (ideally want pi/2 unless out of reach)
+    for angle in eas:
+        ctheta3 = ((dr - L5*np.cos(angle) - deltaY*np.sin(angle))**2 + (dz+L5*np.sin(angle)-deltaY*np.cos(angle)-L1-L2)**2 - L3**2 - L4**2) \
+            / (2*L3*L4)
+        # Check if position is within reach
+        if 1-(ctheta3)**2 >= 0:
+            ea = angle
+            break
+
+    # Calculate joint angles
     theta3 = np.arctan2(np.sqrt(1-(ctheta3)**2), ctheta3)
-    theta2 = np.arctan2(dr - L5*np.cos(ea), (dz+L5*np.sin(ea)-L1-L2)) - \
+    theta2 = np.arctan2(dr - L5*np.cos(ea) - deltaY*np.sin(ea), (dz+L5*np.sin(ea)-deltaY*np.cos(ea)-L1-L2)) - \
         np.arctan2(L4*np.sin(theta3), L3+L4*np.cos(theta3))
     theta4 = np.pi/2 + ea - theta2 - theta3
     theta1 = np.arctan2(dx, -dy)
     thetas = [theta1,theta2,theta3,theta4]
     print(thetas)
 
+    # Use forward kinematics to test positions if wanted
     # List of screws
     Blist = np.array([[0, 0, 1, -deltaY, 0, 0],
                       [1, 0, 0, 0, -L3-L4-L5, deltaY],
@@ -74,40 +65,5 @@ def inverse_kin_analytical2():
     newTheta = np.array(thetas)
     desired = mr.FKinBody(M, Blist, newTheta)
     print(desired)
-    
-def inverse_kin_analytical3():
-    # Ease of notation
-    dx, dy, dz = desired_pos
-    dr = desired_r
-    ea = np.pi/2
-    # cos theta_3
-    ctheta3 = ((dr - L5*np.cos(ea))**2 + (dz+L5*np.sin(ea)-L1-L2)**2 - L3**2 - L4**2) \
-        / (2*L3*L4)
-    theta3 = np.arctan2(np.sqrt(1-(ctheta3)**2), ctheta3)
-    theta2 = np.arctan2(dr - L5*np.cos(ea), (dz+L5*np.sin(ea)-L1-L2)) - \
-        np.arctan2(L4*np.sin(theta3), L3+L4*np.cos(theta3))
-    theta4 = np.pi/2 + ea - theta2 - theta3
-    theta1 = np.arctan2(dx, -dy)
-    thetas = [theta1,theta2,theta3,theta4]
-    print(thetas)
 
-    # List of screws
-    Blist = np.array([[0, 0, 1, -deltaY, 0, 0],
-                      [1, 0, 0, 0, -L3-L4-L5, deltaY],
-                      [1, 0, 0, 0, -L4-L5, deltaY],
-                      [1, 0, 0, 0, -L5, deltaY]]).T
-    # Home configuration
-    M = np.array([[1, 0, 0, 0],
-                  [0, 1, 0, deltaY],
-                  [0, 0, 1, L1+L2+L3+L4+L5],
-                  [0, 0, 0, 1]])
-    
-    # Check Forward Kin with thetas
-    newTheta = np.array(thetas)
-    desired = mr.FKinBody(M, Blist, newTheta)
-    print(desired)
-#pub_joints = rospy.Publisher('desired_joints', List)
-
-#inverse_kin_analytical()
-#inverse_kinematics()
-inverse_kin_analytical3()
+inverse_kin_analytical()
