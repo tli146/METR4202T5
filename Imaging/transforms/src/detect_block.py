2dcddab78dd4f2e91c01341fac92781386bde73c
Tony Li <tli146@hotmail.com>
2022-10-21 08:25:48 +1000

    added detect_block calibration

--- Imaging/transforms/src/detect_block.py
+++ Imaging/transforms/src/detect_block.py
@@ -1,14 +1,16 @@
 from matplotlib.pyplot import thetagrids
 import rospy
+import math
+import heapq
+from scipy.spatial.transform import Rotation as R
 import numpy as np
 import modern_robotics as mr
 from block.msg import Block
 from fiducial_msgs.msg import FiducialTransform, FiducialTransformArray
-
 from geometry_msgs.msg import Transform
 from std_msgs.msg import Header, String, int8, int16, bool
 
-
+calibration_ID = 10
 
 class DetectedBlock:
     def __init__(self, id, x,y,z,theta,color ) -> None:
@@ -28,6 +30,29 @@ class DetectedBlock:
         Block.theta = self.theta
         return Block
 
+    def _is_valid_operand(self, other):
+        return (hasattr(other, "priority"))
+
+    def __eq__(self, other):
+        if not self._is_valid_operation(other):
+            return NotImplemented
+        return self.priority == other.priority
+
+    
+    def __lt__(self, other):
+        if not self._is_valid_operation(other):
+            return NotImplemented
+        return self.priority < other.priority
+
+    def __gt__(self, other):
+        if not self._is_valid_operation(other):
+            return NotImplemented
+        return self.priority > other.priority
+
+
+
+
+
     
 
 
@@ -36,10 +61,10 @@ class DetectedBlock:
 
 class DetectBlock:
 
-    def __init__(self, serial):
+    def __init__(self):
         self.pub =rospy.Publisher(
-        'Trx', # Topic name
-        Transform, # Message type
+        'priority_block', # Topic name
+        Block, # Message type
         queue_size=10 # Topic size (optional)
         ) 
 
@@ -60,10 +85,33 @@ class DetectBlock:
         self.Trx = np.eye(4)
 
         self.transformsList = []
+        self.blockList = []
+        heapq.heapify()
 
     def detection_callback(fiducialTransformArray: FiducialTransformArray):
         self.transformList = fiducialTransformArray.transforms
 
+
+    def calibrate(self, Transf: Transform):
+        rotQ = Transf.rotation
+        transQ = Transf.translation
+        rotM = R.as_matrix(R.from_quat([rotQ.x,rotQ.y,rotQ.z,rotQ.w]))
+        transM = np.arry([transQ.x, transQ.y, transQ.z])*1000
+        Tx = mr.RpToTrans(rotM, transM)
+        self.Trx = mr.TransInv(Tx*mr.TransInv(self.Tr))
+        self.calibrated = True
+        
+    def initialCalibration(self):
+        if self.transformsList == None:
+            return "No aruco cubes detected"
+        for fiducial in self.transformsList:
+            if(fiducial.fiducial_id == calibration_ID):
+                self.calibrate(fiducial.transform)
+                return ("successfully calibrated. Trx =", self.Trx)
+        return "calibration ID not found"
+
+
+        
 
 
    
@@ -78,9 +126,11 @@ if __name__ == '__main__':
     rospy.init_node('detect_block')
     #set frequency to increase performance
     rate = rospy.Rate(10)
-
+    detectBlock = DetectBlock()
+    while not detectBlock.calibrated:
+        detectBlock.initialCalibration()
     while not rospy.is_shutdown():
         #publish message
         rate.sleep()
-    rospy.spin()
+
 
