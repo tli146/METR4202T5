5f4cdc731391a49905220b324bfe67d3bd6ad5a5
etwaugh <etan.waugh@gmail.com>
2022-10-22 22:36:56 +1000

    priority block working, todo rotation

--- metr4202_global_topics/state.py
+++ metr4202_global_topics/state.py
@@ -2,15 +2,31 @@ import rospy
 
 from std_msgs.msg import Int16
 
+
+
+def boostState(data):
+    global state 
+    state = data
+
+    
 def main():
     # Initialise node to publish
     rospy.init_node('state_publisher')
 
+    global state 
+    state = 1
     # Publish state topic
     pub = rospy.Publisher(
         'metr4202_state',
         Int16,
+        
         queue_size=10
+    )
+
+    sub = rospy.Subscriber(
+        'metr4202_state',
+        Int16,
+        boostState
     )
 
     # Set starting state to 1
