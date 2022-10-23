277afcc56f78a07e2036a3a133673131c0eb832d
Tony <tli146@hotmail.com>
2022-10-24 03:59:12 +1000

    made color detect async

--- Imaging/ximea/ximea_color/src/camera.py
+++ Imaging/ximea/ximea_color/src/camera.py
@@ -7,7 +7,8 @@ from sensor_msgs.msg import Image
 from std_msgs.msg import ColorRGBA
 from cv_bridge import CvBridge, CvBridgeError
 
-img = None
+
+ros_rate = 10
 
 class CameraViewer:
 
@@ -16,32 +17,43 @@ class CameraViewer:
     self.serial = serial
     self.image_sub = rospy.Subscriber(f"/ximea_ros/ximea_{self.serial}/image_raw", Image, self.callback)
     self.color_pub = rospy.Publisher("/test_color", ColorRGBA, queue_size=10)
+    self.img = None
 
   def callback(self,data):
-    global img
     try:
-      img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      self.img = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
 
+    
+
+  def publish_color_img(self):
+    img = self.img
     bgr = img[img.shape[0] // 2, img.shape[1] // 2, :]
     color = ColorRGBA()
     color.r = bgr[2]
     color.g = bgr[1]
     color.b = bgr[0]
-    viewer.color_pub.publish(color)
+    self.color_pub.publish(color)
+
+
 
 
 
 if __name__ == '__main__':  
   rospy.init_node('image_node', anonymous=True)
   viewer = CameraViewer('31706251')
+  rate = rospy.Rate(ros_rate)
   try:
     while not rospy.is_shutdown():
-        if img is not None:
+        #if img is not None:
             #cv2.imshow("Image", img)
             #cv2.waitKey(1)
-            None
+            #None
+      viewer.publish_color_img()
+      rate.sleep()
+        
+
   except KeyboardInterrupt:
     print("Shutting down")
   cv2.destroyAllWindows()
