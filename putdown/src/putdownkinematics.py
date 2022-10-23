a4d94c13a2a5b76ed77589fb25ee69e7c2d4a092
LeoXD233 <974637429@qq.com>
2022-10-23 21:14:16 +1000

    add some comments

--- putdown/src/putdownkinematics.py
+++ putdown/src/putdownkinematics.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python3
-"""
-This script publishes a set of random joint states to the dynamixel controller.
-Use this to get an idea of how to code your inverse kinematics!
-"""
 
 # Always need this
+# from glob import glob
+# from msilib.schema import PublishComponent
 import rospy
 
 # Import Numpy and Modern_Robotics and pigpio
 import numpy as np
 import modern_robotics as mr
-import pigpio
+import pigpio               # this import can generate PWN signal
 
 # Import message types
-from std_msgs.msg import Header
-from grip_set.msg import putdown_state
-from grip_set.msg import gripperset
-from sensor_msgs.msg import JointState
-from sensor_msgs.msg import blockColor
-from sensor_msgs.msg import state
-from geometry_msgs.msg import Pose
+from std_msgs.msg import Header,Int16           #✔
+# from grip_set.msg import putdown_state
+# from grip_set.msg import gripperset
+from metr4202_state import current_state        #✔
+from sensor_msgs.msg import JointState          #✔
+from ximea_color_detect.cpp import ColorRGBA   # necessary import color info #✔
+#from sensor_msgs.msg import state
+#from geometry_msgs.msg import Pose          # necessary#✔
 
-def putdownkinematics(pose: Pose) -> JointState:
-    global pub
-
-    
+def putdownkinematics() -> JointState: #generate output type which is jointstate// pub JointState
+    global pub      # variable  which carry the info of JointState and position
+    global state1    # state info from topic 'state'
+    # global blockColor 
     """ ROBOT DIMENSION CONSTANTS (mm)"""
     L1 = 50
     L2 = 51
@@ -32,28 +31,15 @@ def putdownkinematics(pose: Pose) -> JointState:
     L4 = 95
     L5 = 95
     deltaY = 17
-    if state == "True":
-        if blockColor == 0:
-            desired_pos = [100, 10, 60]
-        elif blockColor == 1:
-            desired_pos = [80, 120, 60]
-        elif blockColor ==2:
-            desired_pos = [-80, 120, 60]
-        elif blockColor ==3:
-            desired_pos = [-100, 10, 60]
-    # subscribe for this
-    # neutral pos
-    #desired_pos = [0, -100, 100]
-    # dropoff 1
-    #desired_pos = [100, 10, 60]
-    # dropoff 2
-    #desired_pos = [80, 120, 60]
-    # dropoff 3
-    #desired_pos = [-80, 120, 60]
-    # dropoff 4
-    #desired_pos = [-100, 10, 60]
-    # test pos
-    #desired_pos = [0, -250, 60]
+    if state1 == 6:          # if program's state change to 6, start calculating the info where the cube will putdown
+        if currentcolor == 0: # different color correspond different zone
+            desired_pos = [100, 10, 60] # desired_pos of zone1
+        elif currentcolor == 1:
+            desired_pos = [80, 120, 60] # desired_pos of zone2
+        elif currentcolor ==2:
+            desired_pos = [-80, 120, 60] # desired_pos of zone3
+        elif currentcolor ==3:
+            desired_pos = [-100, 10, 60] # desired_pos of zone4
 
     # desired x,y and z (ease of notation)
     dx, dy, dz = desired_pos
@@ -95,66 +81,80 @@ def putdownkinematics(pose: Pose) -> JointState:
 
     ]
 
-    rospy.loginfo(f'Got desired pose\n[\n\tpos:\n{pose.position}\nrot:\n{pose.orientation}\n]')
-    pub.publish(msg)
+    #rospy.loginfo(f'Got desired pose\n[\n\tpos:\n{pose.position}\nrot:\n{pose.orientation}\n]')
+    pub.publish(msg) # prepare publish msg with type of JointState
     rospy.sleep(4)
 
+def gripper_set(value):           # pub gripper_set
+    global gripperPub   # variable contain info of PWN set of gripper
+    global statePub     # state info pub to topic "state" to change the whole program to next state  
 
+    # no usage
+    # rpi = pigpio.pi()   # use rpi package to control motor
+    # rpi.set_mode(18, pigpio.OUTPUT)
+    # rpi.set_servo_pulsewidth(18,value)
+    #1000 is the closed position
+    #1500 is the grip box position
+    #2000 is the open position 
 
-def main():
+    gripperPub.publish(2000)     # msg published as name "gripperPub"
+    
+    rospy.sleep(0.5)            # after 0.5s change state to 0, at this moment, the gripper is already open
+    statePub = rospy.Publisher('state', Int16)   #statePub is the message which pub to topic 'state'
+    statePub.publish(0)  # msg pub to 'state' is 0
+
+def callback_state(current_state: Int16):       # sub current_state
+    global state1
+    state1 = current_state.data  # receive the state info
+
+def callback_color(ColorRGBA: Int16):       # sub current_state
+    global currentcolor
+    currentcolor = ColorRGBA                
+    putdownkinematics()                     # use the color info to run putdown.py
+
+def main ():
     """ Main loop """
-    global pub
+    global pub 
+    global state1 
     global gripperPub
-    # Initialise node with any node name
+    global statePub
+        # Initialise node with any node name
     rospy.init_node('metr42025_putdownkinematics')
 
-    # Create publisher
+    # creat subscriber of state
+    current_state = rospy.Subscriber(
+        "metr4202_state", # Topic name
+        Int16, # type of info
+        callback_state # callback function of state
+    )
+
+    # create subscriber of blockColor
+    ColorRGBA = rospy.Subscriber(
+        "detected_color", # get color info from sensor_msgs
+        Int16, # type of info
+        callback_color
+    )
+
+    # create publisher of JointState
     pub = rospy.Publisher(
         'desired_joint_states', # Topic name
         JointState, # Message type
         queue_size=10 # Topic size (optional)
     )
 
-    # Create subscriber
-    sub = rospy.Subscriber(
-        'blockColor', # Topic name
-        blockColor, # Message type
-        putdownkinematics # Callback function (required)
-    )
-
-    # Pub a msg to let the gripper open and block drop
+    # create publisher of gripper
     gripperPub = rospy.Publisher(
         'gripper_set',# Topic name
-        gripperset,# message type
+        Int16,# message type
         queue_size=10 # topic size (optional)
     )
 
-    # pub a msg to tell other program that block has dropped and can run other program now
-    pub = rospy.Publisher(
-        "putdown_state",# Topic name
-        putdown_state,
-        queue_size=10 # Topic Size (optional)
+    # create publisher of state
+    statePub = rospy.Publisher(
+        "metr4202_state",  # Topic name of state
+        Int16, # message type
+        queue_size =10
     )
-
-    # Just stops Python from exiting and executes callbacks
-    rospy.spin()
-
-def gripper_set(value):
-    global gripperPub
-    rpi = pigpio.pi()
-    rpi.set_mode(18, pigpio.OUTPUT)
-    rpi.set_servo_pulsewidth(18,value)
-    #1000 is the closed position
-    #1500 is the grip box position
-    #2000 is the open position 
-    # Create message of type gripset
-    msg = gripperset()
-
-    msg.gripperset = (2000)
-    gripperPub.publish(msg)
-
-    rospy.sleep(0.5)
-
 
 if __name__ == '__main__':
     main()
